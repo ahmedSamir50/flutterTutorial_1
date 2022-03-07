7ae2c49a5da3dbeb8809c0161062619ad95aac32
AAbdElAAL <ahmed.samir.abdelaal@gmail.com>
2022-03-07 17:00:43 +0200

    BMI Finished

--- lib/Pages/Components/BMITask/BMIResultScreen.dart
+++ lib/Pages/Components/BMITask/BMIResultScreen.dart
@@ -1,33 +1,36 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled1/Pages/Components/BMITask/BIMStateModel.dart';
 
 @immutable
-class BMIResultsScreen extends StatelessWidget{
-   final BMIStateModel model;
-   const BMIResultsScreen(this.model ,{Key? key}) : super(key: key);
+class BMIResultsScreen extends StatelessWidget {
+  final BMIStateModel model;
+
+  const BMIResultsScreen(this.model, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(child: Text(model.clientGender.toString())),
-    Expanded(child:Text(model.clientHeight.toString())),
-    Expanded(child:Text(model.clientWight.toString())),
-    Expanded(child:Text(model.clientBMI().toString())),
-    Expanded(child:Text(model.bmiState())),
-
-            ],
-          ),
-          appBar: AppBar(
-            title: const Text(" RESULTS "),
-              leading: IconButton(
+      body:Container(
+        padding: const EdgeInsets.all(40),
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(child: Text(model.clientGender.toString())),
+            Expanded(child: Text(model.clientHeight.toString())),
+            Expanded(child: Text(model.clientWight.toString())),
+            Expanded(child: Text(model.clientBMI().toString())),
+            Expanded(child: Text(model.bmiState())),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+          title: const Text(" RESULTS "),
+          leading: IconButton(
             icon: const Icon(Icons.backpack),
-            onPressed:()=>Navigator.pop(context) ,)),
-        );
+            onPressed: () => Navigator.pop(context),
+          )),
+    );
   }
 }
--- lib/Pages/Components/BMITask/BMISate.dart
+++ lib/Pages/Components/BMITask/BMISate.dart
@@ -1,128 +1,278 @@
-
 import 'package:flutter/material.dart';
 import 'package:untitled1/Pages/Components/BMITask/BIMStateModel.dart';
 import 'package:untitled1/Pages/Components/BMITask/BMIApp.dart';
 import 'package:untitled1/Pages/Components/BMITask/BMIResultScreen.dart';
 
-class BMIState extends State<BMIApp>{
-  static final widgetsBGColor =  Colors.grey[900];
-  static const widgetsBGColorBlue =  Colors.blueAccent;
+class BMIState extends State<BMIApp> {
+  static final widgetsBGColor = Colors.grey[900];
+  static const widgetsBGColorBlue = Colors.blueAccent;
 
-  static const maleFemaleTextStyle = TextStyle(fontSize: 20 , fontWeight: FontWeight.bold);
-  late BMIStateModel stt ;
+  static const maleFemaleTextStyle =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  late BMIStateModel stt;
+
   @override
   void initState() {
-    stt =  BMIStateModel();
+    stt = BMIStateModel();
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context ){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: Builder(
-        builder: (context)=>Scaffold(
+        builder: (context) => Scaffold(
           body: Container(
             padding: const EdgeInsets.all(20),
             child: Column(
               children: [
                 Expanded(
-                    child:  Container(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        children: [
-                          Expanded(child:  Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: stt.clientGender ==
-                                    ClientGender.MALE?
-                                widgetsBGColorBlue: widgetsBGColor
-                            ),
-                            child: GestureDetector(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: const [
-                                  Icon(Icons.male , size: 80,),
-                                  Text("MALE",style: maleFemaleTextStyle,)
-                                ],
-                              ),
-                              onTap: ()=>{
-                                setState(()=>{
-                                  stt.clientGender = ClientGender.MALE
-                                })
-                              },
-                            ),
-                          )),
-                          const SizedBox(width: 20,),
-                          Expanded(child:  Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: stt.clientGender !=
-                                    ClientGender.MALE?
-                                widgetsBGColorBlue: widgetsBGColor
-                            ),
-                            child: GestureDetector(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: const [
-                                  Icon(Icons.female,size: 80),
-                                  Text("FEMALE",style: maleFemaleTextStyle,)
-                                ],
-                              ),
-                              onTap: ()=>{
-                                setState(()=>{
-                                  stt.clientGender = ClientGender.FEMALE
-                                })
-                              },
-                            ),
-                          )),
-
-                        ],
-                      ),
-                    )),
-                Expanded(
                     child: Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
-                          color: widgetsBGColor
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text("HEIGHT",style: maleFemaleTextStyle,),
-                          Row(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: stt.clientGender == ClientGender.MALE
+                                ? widgetsBGColorBlue
+                                : widgetsBGColor),
+                        child: GestureDetector(
+                          child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.baseline,
-                            textBaseline: TextBaseline.alphabetic,
-                            children:  [
-                              Text(stt.clientHeight.toString(),style: maleFemaleTextStyle),
-                              const Text(" ~CM" )
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              Icon(
+                                Icons.male,
+                                size: 80,
+                              ),
+                              Text(
+                                "MALE",
+                                style: maleFemaleTextStyle,
+                              )
                             ],
                           ),
-                          Slider(value: stt.clientHeight.toDouble() ,min: 49 , max: 250,
-                              onChanged: (val)=>{
-                                setState(()=>{
-                                  stt.clientHeight = val.round()
-                                })
-                              })
+                          onTap: () => {
+                            setState(
+                                () => {stt.clientGender = ClientGender.MALE})
+                          },
+                        ),
+                      )),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                          child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: stt.clientGender != ClientGender.MALE
+                                ? widgetsBGColorBlue
+                                : widgetsBGColor),
+                        child: GestureDetector(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              Icon(Icons.female, size: 80),
+                              Text(
+                                "FEMALE",
+                                style: maleFemaleTextStyle,
+                              )
+                            ],
+                          ),
+                          onTap: () => {
+                            setState(
+                                () => {stt.clientGender = ClientGender.FEMALE})
+                          },
+                        ),
+                      )),
+                    ],
+                  ),
+                )),
+                Expanded(
+                    child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: widgetsBGColor),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "HEIGHT",
+                        style: maleFemaleTextStyle,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(stt.clientHeight.toString(),
+                              style: maleFemaleTextStyle),
+                          const Text(" ~CM")
                         ],
                       ),
-                    )),
-                Expanded(child: Container()),
+                      Slider(
+                          value: stt.clientHeight.toDouble(),
+                          min: 49,
+                          max: 250,
+                          onChanged: (val) => {
+                                setState(() => {stt.clientHeight = val.round()})
+                              })
+                    ],
+                  ),
+                )),
+                Expanded(
+                    child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: widgetsBGColor),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text("Wight", style: maleFemaleTextStyle),
+                            const SizedBox(height: 30),
+                            Row(
+                              children: [
+                                Expanded(
+                                    child: Center(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                        color: Colors.blueAccent,
+                                        shape: BoxShape.circle),
+                                    child: MaterialButton(
+                                        child: const Text("-",
+                                            style: TextStyle(
+                                                fontSize: 30,
+                                                fontWeight: FontWeight.bold)),
+                                        onPressed: () => {
+                                              setState(() => stt.clientWight--)
+                                            }),
+                                  ),
+                                )),
+                                Expanded(
+                                    child: Center(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                        color: Colors.grey,
+                                        shape: BoxShape.rectangle),
+                                    child: MaterialButton(
+                                        child: Text(stt.clientWight.toString(),
+                                            style: const TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold)),
+                                        onPressed: () => {}),
+                                  ),
+                                )),
+                                Expanded(
+                                    child: Center(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                        color: Colors.blueAccent,
+                                        shape: BoxShape.circle),
+                                    child: MaterialButton(
+                                        child: const Text("+",
+                                            style: TextStyle(
+                                                fontSize: 30,
+                                                fontWeight: FontWeight.bold)),
+                                        onPressed: () => {
+                                              setState(() => stt.clientWight++)
+                                            }),
+                                  ),
+                                )),
+                              ],
+                            )
+                          ],
+                        ),
+                      )),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                          child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: widgetsBGColor),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text("Age", style: maleFemaleTextStyle),
+                            const SizedBox(height: 30),
+                            Row(
+                              children: [
+                                Expanded(
+                                    child: Center(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                        color: Colors.blueAccent,
+                                        shape: BoxShape.circle),
+                                    child: MaterialButton(
+                                        child: const Text("-",
+                                            style: TextStyle(
+                                                fontSize: 30,
+                                                fontWeight: FontWeight.bold)),
+                                        onPressed: () =>
+                                            {setState(() => stt.clientAge--)}),
+                                  ),
+                                )),
+                                Expanded(
+                                    child: Center(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                        color: Colors.grey,
+                                        shape: BoxShape.rectangle),
+                                    child: MaterialButton(
+                                        child: Text(stt.clientAge.toString(),
+                                            style: const TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold)),
+                                        onPressed: () => {}),
+                                  ),
+                                )),
+                                Expanded(
+                                    child: Center(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                        color: Colors.blueAccent,
+                                        shape: BoxShape.circle),
+                                    child: MaterialButton(
+                                        child: const Text("+",
+                                            style: TextStyle(
+                                                fontSize: 30,
+                                                fontWeight: FontWeight.bold)),
+                                        onPressed: () =>
+                                            {setState(() => stt.clientAge++)}),
+                                  ),
+                                )),
+                              ],
+                            )
+                          ],
+                        ),
+                      )),
+                    ],
+                  ),
+                )),
                 MaterialButton(
                     color: Colors.red,
                     minWidth: double.infinity,
                     height: 50,
-                    onPressed: ()=>{
-                      Navigator.push(context,
-                          MaterialPageRoute(builder:
-                              (context)=>BMIResultsScreen(stt)
-                          ) )
-                    },child: Text("CALCULATE ! "))
-
+                    onPressed: () => {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => BMIResultsScreen(stt)))
+                        },
+                    child: Text("CALCULATE ! "))
               ],
             ),
           ),
@@ -130,5 +280,4 @@ class BMIState extends State<BMIApp>{
       ),
     );
   }
-
 }
